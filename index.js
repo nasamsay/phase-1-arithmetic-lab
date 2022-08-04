a75fae14c0ef935de9dc84388887537f3c3194ad
Nasam Sayour <nasamsay@gmail.com>
2022-08-04 17:09:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const num1 =2;
+const num2 =31;
 
-// Write your code here
+let multiply = num1 * num2;
+
+
+let random = Math.floor(Math.random()*10) +1
+
+const num3 =34
+const num4 =10
+let mod = num3 % num4;
+
+let numSet = (12, 4, 20)
+let max = Math.max(numSet)
